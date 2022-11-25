101b80b83643ee43be856e1cc2c4c0ac4fddef3f
Eduardo González <codexlynx@gmail.com>
2022-11-25 17:54:40 +0000

    feat(sensor-catalog): Add sensor-catalog endpoint

---
+++ pkg/client/sensor_catalog.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type SensorType struct {
+	SensorType    int32  `json:"sensor_type"`
+	Manufacturer  string `json:"manufacturer"`
+	ProductName   string `json:"product_name"`
+	ProductNumber string `json:"product_number"`
+	Category      string `json:"category"`
+}
+
+type SensorCatalog struct {
+	SensorTypes []SensorType `json:"sensor_types"`
+}
+
+func (w *weatherlink) SensorCatalog() ([]SensorType, error) {
+	url, err := w.url("sensor-catalog", nil)
+	if err != nil {
+		return []SensorType{}, err
+	}
+	resp, err := w.client.Get(url)
+	result, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return []SensorType{}, err
+	}
+	var data SensorCatalog
+	err = json.Unmarshal(result, &data)
+	if err != nil {
+		return []SensorType{}, err
+	}
+	return data.SensorTypes, nil
+}
